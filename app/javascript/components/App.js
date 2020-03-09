6e3bcd59fd4560728fceac01762bf94efada2b86
Edouard Garret <edouard.garret@gmail.com>
2020-03-09 15:14:49 +0100

    separating app and contact form components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 class App extends React.Component {
+  newContact = (event) => {
+    event.preventDefault();
+    this.props.history.push(`/new-contact`)
+  }
   render () {
-    const newContact = (event) => {
-      event.preventDefault();
-      this.props.history.push(`/new-contact`)
-    }
     return (
       <div>
         <h1>Your contacts!</h1>
-        <input type="submit" onClick={newContact} />
+        <input type="submit" onClick={this.newContact} />
       </div>
     )
   }
--- app/javascript/components/NewContactForm.js
+++ app/javascript/components/NewContactForm.js
@@ -1,1 +1,45 @@
-NewContactForm.js
+import React from 'react'
+
+class NewContactForm extends React.Component {
+  state = {
+    contacts: {}
+  }
+
+  first_nameRef = React.createRef();
+  last_nameRef = React.createRef();
+  emailRef = React.createRef();
+  phone_numberRef = React.createRef();
+
+  createContact = (event) => {
+    {/* preventing default behavior */}
+    event.preventDefault();
+    {/* create new contact */}
+    const contact = {
+      first_name: this.first_nameRef.current.value,
+      last_name: this.last_nameRef.current.value,
+      email: this.emailRef.current.value,
+      phone_number: this.phone_numberRef.current.value,
+    }
+    // copy of state
+    const contacts = {...this.state.contacts}
+    // adding the contact with unique ID
+    contacts[`contact${Date.now()}`] = contact
+    // update state
+    this.setState({ contacts })
+    {/* redirecting to home */}
+  }
+  render () {
+    return (
+      <form action="" className="new-contact-form" onSubmit={this.createContact}>
+        <h2>Add a new contact</h2>
+        <input name="first_name" ref={this.first_nameRef} type="text" placeholder="first name" required/>
+        <input name="last_name" ref={this.last_nameRef} type="text" placeholder="last name" required/>
+        <input name="email" ref={this.emailRef} type="text" placeholder="email" required/>
+        <input name="phone_number" ref={this.phone_numberRef} type="text" placeholder="phone number" required/>
+        <input type="submit" value="Create Contact"/>
+      </form>
+    )
+  }
+}
+
+export default NewContactForm
