2146561ac2df40e022b80602459d0c5e64e0d014
Edouard Garret <edouard.garret@gmail.com>
2020-03-09 11:47:26 +0100

    adding router with home route

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,12 +3,12 @@
 // of the page.
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../components/App'
+import { render } from 'react-dom'
+import Router from '../components/Router'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App />,
+  render(
+    <Router />,
     document.body.appendChild(document.createElement('div')),
   )
 })
