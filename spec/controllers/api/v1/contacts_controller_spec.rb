8a88c5ff662ca3de4b18203dc604cad6d65b919e
Edouard Garret <edouard.garret@gmail.com>
2020-03-11 20:55:47 +0100

    failing at post on create

--- spec/controllers/api/v1/contacts_controller_spec.rb
+++ spec/controllers/api/v1/contacts_controller_spec.rb
@@ -18,4 +18,42 @@ RSpec.describe Api::V1::ContactsController, type: :controller do
       expect(json.length).to eq(5)
     end
   end
+  describe 'GET show' do
+    it 'returns http success response' do
+      contact = create(:contact)
+      get :show, params: { id: contact.id }
+      expect(response.status).to eq 200
+    end
+    it 'returns the right contact details' do
+      contact = create(:contact)
+      get :show, params: { id: contact.id }
+      json = JSON.parse(response.body)
+      expect(json['first_name']).to eq('John')
+      expect(json['last_name']).to eq('Doe')
+      expect(json['email']).to eq('john.doe@gmail.com')
+      expect(json['phone_number']).to eq('123456789')
+    end
+  end
+  describe 'POST create' do
+    before(:each) do
+      contact = build(:contact)
+      contact_params = {
+        contact: {
+          first_name: contact.first_name,
+          last_name: contact.last_name,
+          email: contact.email,
+          phone_number: contact.phone_number
+        }
+      }
+      post :create, params: { contact: contact_params }
+    end
+    it 'returns http success response' do
+      json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+    end
+    it 'adds a user to the database' do
+      contact = Contact.all.size
+      expect(contact).to eq(contact + 1)
+    end
+  end
 end
