4d42ba3f55b9f9bdabf5232edc024b15bb7c1934
Edouard Garret <edouard.garret@gmail.com>
2020-03-11 10:41:18 +0100

    testing presence of all fields

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Contact Data Presence' do
+    before(:each) do
+      @contact = Contact.new(
+        first_name: 'John',
+        last_name: 'Bobson',
+        email: 'a@a.a',
+        phone_number: '123456789'
+      )
+    end
+    it 'is not valid without a first_name' do
+      @contact.first_name = nil
+      expect(@contact).to_not be_valid
+    end
+    it 'is not valid without a last_name' do
+      @contact.last_name = nil
+      expect(@contact).to_not be_valid
+    end
+    it 'is not valid without an email' do
+      @contact.email = nil
+      expect(@contact).to_not be_valid
+    end
+    it 'is not valid without a phone_number' do
+      @contact.phone_number = nil
+      expect(@contact).to_not be_valid
+    end
+  end
 end
