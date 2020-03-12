eb5336607399944d4884a89b15cf338e07ca9a9e
Edouard Garret <edouard.garret@gmail.com>
2020-03-12 18:29:08 +0100

    centering error message when on wrong path

--- app/javascript/components/NotFound.js
+++ app/javascript/components/NotFound.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import BackHome from './BackHome';
 
 const NotFound = () => (
-  <h1>Oups..</h1>
+  <div className="not-found">
+    <div className="center-page">
+      <h1>Oups..</h1>
+      <BackHome />
+    </div>
+  </div>
 )
 
 export default NotFound
